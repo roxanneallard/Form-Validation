d61d80096a17b077cc554726405b09416276502e
roxanneallard <info@roxanneallard.nl>
2016-11-03 14:06:04 +0100

    start
    start

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$("#validationForm").submit(function(event){
+
+  var errorMessage="";
+
+  event.preventDefault();
+
+  function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
+
+if (!isValidEmailAddress($("#emailadres").val())) {
+
+    errorMessage="<br>Vul a.u.b. een geldig e-mailadres in.";
+
+  }
+
+  if (!$.isNumeric($("#telefoonnummer").val())) {
+
+      errorMessage=errorMessage+"<br>Vul a.u.b. een geldig telefoonnummer in.";
+
+    }
+
+if ($("#wachtwoord").val() != $("#wachtwoordbevestigen").val()) {
+errorMessage=errorMessage+"<br />Vul a.u.b. hetzelfde wachtwoord in";
+}
+
+      if (errorMessage=="") {
+        alert("uw data is verzonden");
+      } else {
+
+    $("#error").html(errorMessage);
+  }
+
+});
